760469242425e653f2a6dc69eb4effbf3e6bd8f2
mondalaci <mondalaci@e0e88c14-cc10-0410-a3b2-c083e06921da>
2003-02-15 00:51:33 +0000

    draw_hline prototype added because of its replacement from panel.c to color.h

--- uc/color.h
+++ uc/color.h
@@ -4,5 +4,7 @@
 #define getpair(fg, bg) COLOR_PAIR(bg*8+fg)
 
 void init_color_pairs();
+/* maybe `draw_hline' is not closely belongs here but I think it's a good place */
+void draw_hline(int y, int x, int h);
 
 #endif /* __COLOR_H */
