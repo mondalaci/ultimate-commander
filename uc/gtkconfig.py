fc1a308d5b38fdf91627b27933760bc323c8b530
mondalaci <mondalaci@e0e88c14-cc10-0410-a3b2-c083e06921da>
2003-08-05 21:05:05 +0000

    `encoding' added.

--- uc/gtkconfig.py
+++ uc/gtkconfig.py
@@ -1,5 +1,9 @@
-## GTK user interface settings
+## GTK2 user interface settings
 
+# encoding used for accentuated filenames
+encoding = 'iso-8859-2'
+
+# panel colors
 gtk_panel_background = '#000000'
 gtk_panel_foreground = '#ffffff'
 gtk_panel_tagged = '#444400'
