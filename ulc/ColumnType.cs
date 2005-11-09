fe3a00b4027aa7448d1436d252d5e45a280942ca
mondalaci <mondalaci@e0e88c14-cc10-0410-a3b2-c083e06921da>
2005-11-09 01:29:14 +0000

    Restart in C#

---
+++ ulc/ColumnType.cs
@@ -0,0 +1,73 @@
+using System;
+using Mono.Posix;
+
+namespace UltimateCommander {
+
+	class ColumnType {
+
+		public enum Type {
+			Icon,
+			Name,
+			Size,
+			CTime,
+			MTime,
+			ATime,
+			Permission,
+			Uid,
+			Gid,
+			Description,
+			MimeType,
+			FileType,
+			INode,
+			ExtendedAttribute,
+			MetaTag
+		}
+
+		public enum FileTypeArg {
+			LetterType,
+			SymbolicType
+		}
+
+		public enum NameArg {
+			ShortName,
+			FullPath
+		}
+
+		public enum SizeArg {
+			Byte,
+			Kib,
+			Kb,
+			Mib,
+			Mb
+		}
+
+		// for both UIDs and GIDs
+		public enum IdArg {
+			Name,
+			Num
+		}
+
+		Type type;
+		int arg;
+
+		public ColumnType(Type t, int a) {
+			type = t;
+			arg = a;
+		}
+
+		public string GetContent(File file) {
+			switch (type) {
+			case Type.FileType:
+				return "x";
+			case Type.Name:
+				if (arg == (int)NameArg.ShortName)
+					return file.FileName;
+				else if (arg == (int)NameArg.FullPath)
+					return file.FullPath;
+				else {}
+				break;
+			}
+			return "unknown";
+		}
+	}
+}
---
+++ ulc/UltimateCommander.cs
@@ -0,0 +1,71 @@
+using System;
+
+using Gtk;
+using Gdk;
+
+namespace UltimateCommander {
+
+	public class UltimateCommander {
+
+		static Panel panel1;
+		static Panel panel2;
+		static HPaned hpaned;
+
+     	public static void Main (string [] args)
+     	{              
+          	Application.Init();
+                        
+			string default_directory = ".";
+
+			panel1 = new Panel(default_directory);
+			panel1.ActivatedEvent += new ActivatedHandler(OnPanelActivated);
+
+			panel2 = new Panel(default_directory);
+			panel2.ActivatedEvent += new ActivatedHandler(OnPanelActivated);
+
+			hpaned = new HPaned();
+			hpaned.Add1(panel1);
+			hpaned.Add2(panel2);
+			hpaned.Position = 500;
+			hpaned.MoveHandle += new MoveHandleHandler(OnMoveHandle);
+          	Gtk.Window window = new Gtk.Window("Ultimate Commander");
+          	window.SetDefaultSize(1000, 700);
+          	window.DeleteEvent += new DeleteEventHandler(OnDeleteEvent);
+			window.Add(hpaned);
+			//window.ResizeChecked += new EventHandler(OnMainWindowResizeChecked);
+          	window.ShowAll();
+
+          	Application.Run ();
+		}
+
+		private static void OnPanelActivated(Panel panel)
+		{
+			Panel passive_panel;
+			
+			if (panel == panel1)
+				passive_panel = panel2;
+			else
+				passive_panel = panel1;
+				
+			passive_panel.SetActivated(false);
+		}
+
+		[GLib.ConnectBefore]
+		private static void OnMainWindowResizeChecked(object o, EventArgs args)
+		{
+			float ratio =  (float)hpaned.Position / (float)hpaned.Allocation.Width;
+			Console.WriteLine("resized {0}", ratio);
+		}
+
+		[GLib.ConnectBefore]
+		private static void OnMoveHandle(object o, MoveHandleArgs args)
+		{
+			Console.WriteLine("MoveHandle");
+		}
+
+     	private static void OnDeleteEvent (System.Object o, DeleteEventArgs args)
+     	{
+          	Application.Quit();
+     	}
+	}
+}
