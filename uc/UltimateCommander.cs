2d229bba9c4db022b5cdc64bbc25f3c194e475cd
mondalaci <mondalaci@e0e88c14-cc10-0410-a3b2-c083e06921da>
2006-02-28 17:14:36 +0000

    Check the UC_GUI_PATH environment variable and look up GUI resources accrodingly to the path it denotes.

--- uc/UltimateCommander.cs
+++ uc/UltimateCommander.cs
@@ -6,11 +6,20 @@ namespace UltimateCommander {
 
 	public class UltimateCommander {
 
-		public static string GladeFileName = "gui/uc.glade";
+		static string program_gui_path;
+		public static string GladeFileName;
 		public static MainWindow MainWindow;
 
      	public static void Main(string[] args)
      	{              
+			program_gui_path = Environment.GetEnvironmentVariable("UC_GUI_PATH");
+
+			if (program_gui_path == null) {
+				program_gui_path = "gui";
+			}
+
+			GladeFileName = program_gui_path + "/uc.glade";
+
 			Gnome.Vfs.Vfs.Initialize();
           	Gtk.Application.Init();
 			MainWindow = new MainWindow();
